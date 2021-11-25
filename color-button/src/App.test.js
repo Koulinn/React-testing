4914daa619b10a97919ce6e8bdea1f53c2bdca53
Koulinn <drdverzola@gmail.com>
2021-11-25 13:53:31 +0000

    first test check color

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,11 +1,47 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  //looks in DOM by any element displayed as the regex on getByRole
-  // role are based on role by W3 you can add role to div for example
-  const linkElement = screen.getByRole('link', { name: /learn react/i });
-  //assertion expression, test expression
-  expect(linkElement).toBeInTheDocument();
-});
+
+
+test('btn has correct initial color', () => {
+  render(<App />)
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+
+})
+
+test('btn has correct initial text', () => {
+
+})
+test('btn turns blue when clicked', () => {
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   //looks in DOM by any element displayed as the regex on getByRole
+//   // role are based on role by W3 you can add role to div for example
+//   const linkElement = screen.getByRole('link', { name: /learn react/i });
+//   //assertion expression, test expression
+//   expect(linkElement).toBeInTheDocument();
+// });
