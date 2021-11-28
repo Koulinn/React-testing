444cf70e648d1167d21a4692a6349c38cd89d924
Koulinn <drdverzola@gmail.com>
2021-11-28 09:43:13 +0000

    updated url path to follow the config

--- sundaes-app/src/mocks/handlers.js
+++ sundaes-app/src/mocks/handlers.js
@@ -1,8 +1,12 @@
 import { rest } from 'msw'
+import config from '../config'
 
+const { SERVER_URL } = config
+
+console.log(SERVER_URL)
 
 export const handlers = [
-    rest.get("http://localhost:3030/scops", (req, res, ctx) => {
+    rest.get(`${SERVER_URL}scoops`, (req, res, ctx) => {
         return res(
             ctx.json([
                 { name: 'Chocolate', imagePath: '/images/chocolate.png' },
